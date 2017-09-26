fe20978b903d63ebbec3f2d33816f690fbe78340
Emily Bram <emilybbram@gmail.com>
2017-09-25 21:54:12 -0700

    fixed dir paths

--- server/app.js
+++ server/app.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require('path');
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+app.use(express.static(path.resolve(process.env.PWD, 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.resolve(process.env.PWD, 'build', 'index.html'));
 });
 
 module.exports = app;
