bc8ce2b3e7c4614b3fa33dbfd3d24068874ee807
Emily Bram <emilybbram@gmail.com>
2017-09-26 20:19:45 -0700

    server change

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(path.resolve(process.env.PWD, 'build')));
 
 // Return the main index.html
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(process.env.PWD, 'build', 'index.html'));
 });
 
