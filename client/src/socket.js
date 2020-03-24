00e43a010ad2b6d1c4114cf326f3d6cc49cef69d
Emily Bram <emilybbram@gmail.com>
2020-03-23 22:39:01 -0700

    check

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,81 +1,80 @@
-import io from 'socket.io-client';
-import Utils from './utils';
-import Events  from './events';
+import io from "socket.io-client";
+import Utils from "./utils";
+import Events from "./events";
 
 class Socket {
+  static setup(app) {
+    const socket = io("/game");
 
-    static setup(app) { 
-        const socket = io('/game');
+    socket.on(Events.CONNECTED, function(playerId) {
+      app.playerId = playerId;
+    });
 
-        socket.on(Events.CONNECTED, function(playerId){
-            app.playerId = playerId;
-        });
+    socket.on(Events.START_GAME, function({ socketId }) {
+      app.setState({
+        waitingForOpponent: false
+      });
+      app.socket.emit("sendLetters", {
+        socketId,
+        lettersFlipped: app.state.lettersFlipped,
+        lettersUnflipped: app.state.lettersUnflipped
+      });
+    });
 
-        socket.on(Events.READY_TO_START, function({socketId}){
-            app.setState({
-                waitingForOpponent: false
-            });
-            app.socket.emit('sendLetters', {
-                socketId,
-                lettersFlipped: app.state.lettersFlipped,
-                lettersUnflipped: app.state.lettersUnflipped
-            });
-        });
+    socket.on("receiveLetters", function({ lettersFlipped, lettersUnflipped }) {
+      app.setState({
+        lettersFlipped,
+        lettersUnflipped,
+        waitingForOpponent: false
+      });
+    });
 
-        socket.on('receiveLetters', function({lettersFlipped, lettersUnflipped}){
-            app.setState({
-                lettersFlipped,
-                lettersUnflipped,
-                waitingForOpponent: false
-            });
-        });
+    socket.on("firstPlayer", function() {
+      app.setState({
+        lettersUnflipped: Utils.getShuffledLetters(),
+        lettersFlipped: [],
+        yourTurn: true
+      });
+    });
 
-        socket.on('firstPlayer', function(){
-            app.setState({
-                lettersUnflipped: Utils.getShuffledLetters(),
-                lettersFlipped: [],
-                yourTurn: true
-            });
-        });
+    socket.on("word", function(word) {
+      let lettersCurrWord = app.state.lettersCurrWord.slice();
+      let lettersFlipped = app.state.lettersFlipped.slice();
+      let letter;
 
-        socket.on('word', function(word) {
-            let lettersCurrWord = app.state.lettersCurrWord.slice();
-            let lettersFlipped = app.state.lettersFlipped.slice();
-            let letter;
+      for (let i = 0; i < word.length; i++) {
+        letter = word.charAt(i);
+        let idx = lettersFlipped.indexOf(letter);
+        if (idx > -1) {
+          lettersFlipped.splice(idx, 1);
+        } else {
+          lettersFlipped = [...lettersFlipped, ...lettersCurrWord];
+          lettersCurrWord = [];
+          idx = lettersFlipped.indexOf(letter);
+          lettersFlipped.splice(idx, 1);
+        }
+      }
 
-            for (let i = 0; i < word.length; i++) {
-                letter = word.charAt(i);
-                let idx = lettersFlipped.indexOf(letter);
-                if (idx > -1) {
-                    lettersFlipped.splice(idx, 1);
-                } else {
-                    lettersFlipped = [...lettersFlipped, ...lettersCurrWord];
-                    lettersCurrWord = [];
-                    idx = lettersFlipped.indexOf(letter);
-                    lettersFlipped.splice(idx, 1);
-                }
-            }
-            
-            app.setState({
-                opponentWords: [...app.state.opponentWords, word],
-                lettersFlipped,
-                lettersCurrWord
-            });
-        });
+      app.setState({
+        opponentWords: [...app.state.opponentWords, word],
+        lettersFlipped,
+        lettersCurrWord
+      });
+    });
 
-        socket.on('letterFlip', function() {
-            app.flipLetter();
-            app.setState({
-                yourTurn: true
-            });
-        });
+    socket.on("letterFlip", function() {
+      app.flipLetter();
+      app.setState({
+        yourTurn: true
+      });
+    });
 
-        socket.on('endGame', function() {
-            app.onOpponentEndGame();
-        });
+    socket.on("endGame", function() {
+      app.onOpponentEndGame();
+    });
 
-        return socket;
-    }
+    return socket;
+  }
 }
 
 export default Socket;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const Events = require('../client/src/events');
+const Events = require("../client/src/events");
 
-const app = require('./app');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = require("./app");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 9000;
 
@@ -14,49 +14,53 @@ server.listen(PORT, function() {
 
 const games = {};
 
-io.of('/game').on('connection', function(socket){
-    console.log("socket connected");
-    
-    socket.on('gameRoom', function({gameRoom: gameId}){
-        console.log("Player " + socket.id + " joining game " + gameId);
-        socket.gameRoom = gameId
-        socket.join(gameId);
-        socket.emit(Events.CONNECTED, socket.id);
-        if (!games[gameId]) {
-            games[gameId] = [socket.id];
-            socket.emit('firstPlayer');
-        } else {
-            games[gameId].push(socket.id);
-            socket.to(games[gameId][0]).emit(Events.READY_TO_START, {socketId: socket.id});
-        }
-    });
+io.of("/game").on("connection", function(socket) {
+  console.log("socket connected");
 
-    socket.on('sendLetters', function({socketId,
-        lettersFlipped,
-        lettersUnflipped}){
-        console.log("Sending player " + socketId + " letters");
-        socket.to(socketId).emit('receiveLetters', {
-            lettersFlipped,
-            lettersUnflipped
-        });
-    });
+  socket.on("gameRoom", function({ gameRoom: gameId }) {
+    console.log("Player " + socket.id + " joining game " + gameId);
+    socket.gameRoom = gameId;
+    socket.join(gameId);
+    socket.emit(Events.CONNECTED, socket.id);
+    if (!games[gameId]) {
+      games[gameId] = [socket.id];
+      socket.emit("firstPlayer");
+    } else {
+      games[gameId].push(socket.id);
+      socket
+        .to(games[gameId][0])
+        .emit(Events.START_GAME, { socketId: socket.id });
+    }
+  });
 
-    socket.on('word', function(word){
-        console.log("Player " + socket.id + " submitted " + word);
-        socket.to(socket.gameRoom).emit('word', word);
+  socket.on("sendLetters", function({
+    socketId,
+    lettersFlipped,
+    lettersUnflipped
+  }) {
+    console.log("Sending player " + socketId + " letters");
+    socket.to(socketId).emit("receiveLetters", {
+      lettersFlipped,
+      lettersUnflipped
     });
+  });
 
-    socket.on('letterFlip', function(){
-        console.log("Player " + socket.id + " flipped letter");
-        socket.broadcast.to(socket.gameRoom).emit('letterFlip');
-    });
+  socket.on("word", function(word) {
+    console.log("Player " + socket.id + " submitted " + word);
+    socket.to(socket.gameRoom).emit("word", word);
+  });
 
-    socket.on('endGame', function(){
-        console.log("Player " + socket.id + " ended game");
-        socket.broadcast.to(socket.gameRoom).emit('endGame');
-    });
+  socket.on("letterFlip", function() {
+    console.log("Player " + socket.id + " flipped letter");
+    socket.broadcast.to(socket.gameRoom).emit("letterFlip");
+  });
 
-    socket.on('disconnect', function () {
-        console.log("Player " + socket.id + " has disconnected.");
-    });
+  socket.on("endGame", function() {
+    console.log("Player " + socket.id + " ended game");
+    socket.broadcast.to(socket.gameRoom).emit("endGame");
+  });
+
+  socket.on("disconnect", function() {
+    console.log("Player " + socket.id + " has disconnected.");
+  });
 });
