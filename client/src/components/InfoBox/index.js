a742ab947d50e118a8c7e3df1d2f35867260563c
Emily Bram <emilybbram@gmail.com>
2020-03-23 22:27:28 -0700

    check

--- client/src/components/InfoBox/index.js
+++ client/src/components/InfoBox/index.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.css";
 
 const InfoBox = ({
-    yourCount,
-    opponentCount,
-    userEndGame,
-    opponentEndGame,
-    gameOver,
-    yourTurn
-    }) => {
+  yourCount,
+  opponentCount,
+  userEndGame,
+  opponentEndGame,
+  gameOver,
+  yourTurn
+}) => {
+  let message = "";
 
-    let message = "";
+  if (!gameOver && yourTurn) {
+    message = "Your turn!";
+  } else if (!gameOver) {
+    message = "Waiting for opponent...";
+  } else if (!userEndGame) {
+    message = "No more tiles! Press '1' to end game.";
+  } else if (!opponentEndGame) {
+    message = "Waiting for opponent...";
+  } else if (yourCount > opponentCount) {
+    message = "You Win!";
+  } else if (yourCount < opponentCount) {
+    message = "You Lose!";
+  } else {
+    message = "It's a Tie!";
+  }
 
-    if (!gameOver && yourTurn) {
-        message = "Your turn!";
-    } else if (!gameOver) {
-        message = "Waiting for opponent...";
-    } else if (!userEndGame) {
-        message = "No more tiles! Press '1' to end game.";
-    } else if (!opponentEndGame) {
-        message = "Waiting for opponent...";
-    } else if (yourCount > opponentCount) {
-        message = "You Win!";
-    } else if (yourCount < opponentCount) {
-        message = "You Lose!";
-    } else {
-        message = "It's a Tie!";
-    }
-
-    return (
-        <div className="InfoBox">
-            {message}
-        </div>
-    );
-}
+  return <div className="InfoBox">{message}</div>;
+};
 
 export default InfoBox;
